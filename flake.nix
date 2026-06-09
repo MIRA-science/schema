ac5c9d79e7ed56f1e62a85960f497201c8208827
AutoMathis <automathis@protonmail.com>
2026-06-09 16:52:54 +0200

    feat: Add nix config and uv lock

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "MIRA schema development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python314
+            uv
+            nodejs
+            plantuml
+            stdenv.cc.cc.lib
+          ];
+
+          env = {
+            # Prevent uv from downloading its own Python; use the one from Nix
+            UV_PYTHON_DOWNLOADS = "never";
+            UV_PYTHON = "${pkgs.python314}/bin/python3.14";
+            LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+          };
+
+          shellHook = ''
+            uv sync
+          '';
+        };
+      }
+    );
+}
